be1d3bfe240b84daa8aadf6e12d91b7c809b4c83
Hannah <hannah.v.meyer@gmail.com>
2018-01-16 14:40:21 +0000

    Add structure to input description

--- limmbo/io/__init__.py
+++ limmbo/io/__init__.py
@@ -1,19 +1,19 @@
 r"""
 Data input
-**********************
+**********
 
 Parse command-line arguments
-^^^^^^^^^^^^^
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 .. autoclass:: limmbo.io.parser.ParseData
   :members:
 
 Read data
-^^^^^^^^^^^^^^^^^^^
+^^^^^^^^^
 .. autoclass:: limmbo.io.reader.ReadData
   :members:
 
 Check data
-^^^^^^^^^^^^^^^^^^^
+^^^^^^^^^^
 .. autoclass:: limmbo.io.input.InputData
   :members:
 """
