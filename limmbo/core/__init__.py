ffdae3c07c007b768d0c5e937ac4ce478291cfc7
Hannah <hannah.v.meyer@gmail.com>
2018-01-16 14:39:45 +0000

    Add structure to vd description

---
+++ limmbo/core/__init__.py
@@ -0,0 +1,16 @@
+r"""
+LiMMBo
+^^^^^^
+.. autoclass:: limmbo.core.vdbootstrap.LiMMBo
+  :members:
+
+Standard REML
+^^^^^^^^^^^^^
+.. autofunction:: limmbo.core.vdsimple.vd_reml
+
+"""
+
+import vdbootstrap 
+import vdsimple
+
+__all__ = ['vdbootstrap', 'vdsimple']
