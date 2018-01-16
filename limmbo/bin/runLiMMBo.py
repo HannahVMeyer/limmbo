180269a4e3d85a0dfda7647ac9898eb971407b59
Hannah <hannah.v.meyer@gmail.com>
2018-01-16 14:39:14 +0000

    Adapt scripts to changes in input and reader

--- limmbo/bin/runLiMMBo.py
+++ limmbo/bin/runLiMMBo.py
@@ -7,9 +7,12 @@ import h5py
 import sys
 import os
 
-from limmbo.io.parser import DataParse
-from limmbo.io.input import DataInput
-from limmbo.core.vdbootstrap import DataLimmbo
+from limmbo.io.parser import ParseData
+from limmbo.io.reader import ReadData
+from limmbo.io.input import InputData
+from limmbo.core.vdbootstrap import LiMMBo
+
+import pdb
 
 #################
 ### functions ###
@@ -18,23 +21,45 @@ from limmbo.core.vdbootstrap import DataLimmbo
 
 def entry_point():
 
-    # initiate DataParse object
-    dataparse = DataParse()
+    # parse command-line arguments
+    dataparse = ParseData()
     dataparse.getArgs()
 
-    # initiate Data objects
-    datainput = DataInput(dataparse.options)
+    # read data specified in command-line arguments
+    dataread = ReadData(verbose=dataparse.options.verbose)
+    dataread.getPhenotypes(file_pheno = dataparse.options.file_pheno)
+    dataread.getCovariates(file_covariates = dataparse.options.file_covariates)
+    dataread.getRelatedness(file_relatedness = dataparse.options.file_relatedness)
+    
+    # combine all input, check for consistency and pre-process data
+    datainput = InputData(verbose=dataparse.options.verbose)
+    datainput.addPhenotypes(phenotypes = dataread.phenotypes,
+                            phenotype_ID = dataread.phenotype_ID,
+                            pheno_samples = dataread.pheno_samples)
+    datainput.addRelatedness(relatedness = dataread.relatedness,
+                            relatedness_samples = dataread.relatedness_samples)
+    datainput.addCovariates(covariates = dataread.covariates,
+                            covs_samples = dataread.covs_samples)
+    datainput.commonSamples()
+    datainput.subsetTraits(traitstring = dataparse.options.traitstring)
+    datainput.regress(regress = dataparse.options.regress)
+    datainput.transform(type = dataparse.options.transform)
 
-    ### running analyses ###
-    datainput.getPhenotypes()
-    datainput.getRelatedness()
-    #datainput.commonSamples()
-    datainput.subsetTraits()
-    datainput.getCovariates()
-    datainput.regress_and_transform()
-    datalimmbo = DataLimmbo(datainput=datainput, options=dataparse.options)
-    resultsQ = datalimmbo.sampleCovarianceMatricesPP()
-    datalimmbo.combineBootstrap(resultsQ)
+    # set up variance decomposition via LiMMBo
+    datalimmbo = LiMMBo(datainput=datainput,
+            S=dataparse.options.S, 
+            timing=dataparse.options.timing,
+            iterations=dataparse.options.iterations,
+            verbose=dataparse.options.verbose)
+    resultsBS = datalimmbo.runBootstrapCovarianceEstimation(
+            seed=dataparse.options.seed, cpus=dataparse.options.cpus, 
+            minCooccurrence=dataparse.options.minCooccurrence, 
+            n=dataparse.options.runs)
+
+    resultsCovariance = datalimmbo.combineBootstrap(results=resultsBS) 
+    datalimmbo.saveVarianceComponents(resultsCovariance,
+            output=dataparse.options.output,
+            intermediate=dataparse.options.intermediate)
 
 
 #############
--- limmbo/bin/runSimpleVD.py
+++ limmbo/bin/runSimpleVD.py
@@ -4,36 +4,42 @@
 
 import h5py
 
-from limmbo.io.parser import DataParse
-from limmbo.io.input import DataInput
-from limmbo.core.vdsimple import simpleCD
+from limmbo.io.parser import ParseData
+from limmbo.io.reader import ReadData
+from limmbo.io.input import InputData
+from limmbo.core.vdsimple import vd_reml
 
+import pandas as pd
 #################
 ### functions ###
 #################
 def entry_point():
 
     # initiate DataParse object
-    dataparse = DataParse()
+    dataparse = ParseData()
     dataparse.getArgs()
 
     # datareader = DataReader()
+    dataread = ReadData(verbose=dataparse.options.verbose)
+    dataread.getPhenotypes(file_pheno = dataparse.options.file_pheno)
+    dataread.getCovariates(file_covariates = dataparse.options.file_covariates)
+    dataread.getRelatedness(file_relatedness = dataparse.options.file_relatedness)
 
-    # initiate Data objects
-    datainput = DataInput()
-
-
-    ### running analyses ###
-    datainput.getPhenotypes()
-    datainput.getRelatedness()
-    #datainput.commonSamples()
-    datainput.subsetTraits()
-    datainput.getCovariates()
-    datainput.regress_and_transform()
+    # combine all input, check for consistency and pre-process data
+    datainput = InputData(verbose=dataparse.options.verbose)
+    datainput.addPhenotypes(phenotypes = dataread.phenotypes,
+                            phenotype_ID = dataread.phenotype_ID,
+                            pheno_samples = dataread.pheno_samples)
+    datainput.addRelatedness(relatedness = dataread.relatedness,
+                            relatedness_samples = dataread.relatedness_samples)
+    datainput.addCovariates(covariates = dataread.covariates,
+                            covs_samples = dataread.covs_samples)
+    datainput.commonSamples()
+    datainput.subsetTraits(traitstring = dataparse.options.traitstring)
+    datainput.regress(regress = dataparse.options.regress)
+    datainput.transform(type = dataparse.options.transform)
     
-    Cg, Cn, processtime = simpleVD(datainput = datainput, 
-            cache = dataparse.options.cache,
-            output = dataparse.options.output,
+    Cg, Cn, processtime = vd_reml(datainput = datainput, 
             iterations = dataparse.options.iterations,
             verbose = dataparse.options.verbose)
 
@@ -41,14 +47,13 @@ def entry_point():
     try:
         pd.DataFrame(Cg).to_csv('{}/Cg_REML.csv'.format(
             dataparse.options.output), sep=",", header=False, index=False)
-        pd.DataFrame(Cn).to_csv('{}/Cn_RML.csv'.format(
+        pd.DataFrame(Cn).to_csv('{}/Cn_REML.csv'.format(
             dataparse.options.output), sep=",", header=False, index=False)
-        pd.DataFrame([processtime]).to_csv('{}/process_time_REML.',
-            '.csv'.format(dataparse.options.output), sep=",", header=False,
-            index=False)
+        pd.DataFrame([processtime]).to_csv('{}/process_time_REML.csv'.format(
+	    dataparse.options.output), sep=",", header=False, index=False)
     except:
-        raise IOError('Cannot write to {}: check writing permissions',
-            '{}'.format(dataparse.options.output))
+        raise IOError('Cannot write to {}: check writing permissions'.format(
+			dataparse.options.output))
 
 
 ############
