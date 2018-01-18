0927beb93d8168c531a495f9202d450379ff120d
Hannah <hannah.v.meyer@gmail.com>
2018-01-18 22:16:34 +0000

    Move structure to rst file

--- limmbo/core/__init__.py
+++ limmbo/core/__init__.py
@@ -1,15 +1,3 @@
-r"""
-LiMMBo
-^^^^^^
-.. autoclass:: limmbo.core.vdbootstrap.LiMMBo
-  :members:
-
-Standard REML
-^^^^^^^^^^^^^
-.. autofunction:: limmbo.core.vdsimple.vd_reml
-
-"""
-
 import vdbootstrap 
 import vdsimple
 
