412fa108c95b833bd52f00efd948e6898824fe46
Hannah <hannah.v.meyer@gmail.com>
2018-01-25 15:21:00 +0000

    Update unit tests

--- limmbo/io/test/test_input.py
+++ limmbo/io/test/test_input.py
@@ -6,12 +6,6 @@ from limmbo.io.input import InputData
 from limmbo.io.input import MissingInput
 from limmbo.io.input import DataMismatch
 from limmbo.io.input import FormatError
-from limmbo.core.vdbootstrap import LiMMBo
-
-#################
-### functions ###
-#################
-
 
 class Input(unittest.TestCase):
 
@@ -28,174 +22,134 @@ class Input(unittest.TestCase):
         self.relatedness_samples = np.array(('S1','S2'))
 
 
-    def test_getPhenotypes_missing_phenotypes(self):
-        with self.assertRaises(MissingInput):
-	    self.datainput.getPhenotypes()
+    def test_addPhenotypes_missing_phenotypes(self):
+        with self.assertRaises(TypeError):
+	    self.datainput.addPhenotypes()
     
-    def test_getPhenotypes_missing_phenotype_ID(self):
-        with self.assertRaises(MissingInput):
-	    self.datainput.getPhenotypes(phenotypes=self.phenotypes,
+    def test_addPhenotypes_missing_phenotype_ID(self):
+        with self.assertRaises(TypeError):
+	    self.datainput.addPhenotypes(phenotypes=self.phenotypes,
                                          pheno_samples=self.pheno_samples)
     
-    def test_getPhenotypes_missing_pheno_samples(self):
-        with self.assertRaises(MissingInput):
-	    self.datainput.getPhenotypes(phenotypes=self.phenotypes,
+    def test_addPhenotypes_missing_pheno_samples(self):
+        with self.assertRaises(TypeError):
+	    self.datainput.addPhenotypes(phenotypes=self.phenotypes,
                                          phenotype_ID=self.phenotype_ID)
     
-    def test_getPhenotypes_mismatch_phenotype_ID_phenotype_dimension(self):
+    def test_addPhenotypes_mismatch_phenotype_ID_phenotype_dimension(self):
         pheno_ID=np.array(('ID1', 'ID2', 'ID3', 'ID4'))
         with self.assertRaises(DataMismatch):
-	    self.datainput.getPhenotypes(phenotypes=self.phenotypes,
+	    self.datainput.addPhenotypes(phenotypes=self.phenotypes,
                                          phenotype_ID=pheno_ID,
                                          pheno_samples=self.pheno_samples)
     
-    def test_getPhenotypes_mismatch_pheno_samples_phenotype_dimension(self):
+    def test_addPhenotypes_mismatch_pheno_samples_phenotype_dimension(self):
         pheno_samples=np.array(('S1', 'S2', 'S3'))
         with self.assertRaises(DataMismatch):
-	    self.datainput.getPhenotypes(phenotypes=self.phenotypes,
+	    self.datainput.addPhenotypes(phenotypes=self.phenotypes,
                                          phenotype_ID=self.phenotype_ID,
                                          pheno_samples=pheno_samples)
     
 
-    def test_getCovariates_missing_covs_samples(self):
+    def test_addCovariates_missing_covs_samples(self):
         with self.assertRaises(MissingInput):
-	    self.datainput.getCovariates(covariates=self.covariates)
+	    self.datainput.addCovariates(covariates=self.covariates)
     
-    def test_getCovariates_mismatch_covs_samples_covariates_dimension(self):
+    def test_addCovariates_mismatch_covs_samples_covariates_dimension(self):
         covs_samples=np.array(('S1', 'S2', 'S3'))
         with self.assertRaises(DataMismatch):
-	    self.datainput.getCovariates(covariates=self.covariates,
+	    self.datainput.addCovariates(covariates=self.covariates,
                                          covs_samples=covs_samples)
 
-    def test_getCovariates_covariates_type(self):
-        covariates=((1,2),(2,3))
-        with self.assertRaises(TypeError):
-	    self.datainput.getCovariates(covariates=covariates,
-                                         covs_samples=self.covs_samples)
-
-    def test_getCovariates_covs_samples_type(self):
-        covs_samples=['S1', 'S2', 'S3']
-        with self.assertRaises(TypeError):
-	    self.datainput.getCovariates(covariates=self.covariates,
-                                         covs_samples=covs_samples)
-
-    def test_getCovariates_fromOptions_wrongFileFormat(self):
-        self.options = Options()
-        self.datainputFromOptions = DataInput(self.options)
-        with self.assertRaises(FormatError):
-	    self.datainputFromOptions.getCovariates()
-    
-    def test_getCovariates_fromOptions_FileNonExisting(self):
-        self.options = Options(file_covariates="test.csv")
-        self.datainputFromOptions = DataInput(self.options)
-        with self.assertRaises(IOError):
-	    self.datainputFromOptions.getCovariates()
-
-    def test_getRelatedness_missing_relatedness_samples(self):
+    def test_addRelatedness_missing_relatedness_samples(self):
         with self.assertRaises(MissingInput):
-	    self.datainput.getRelatedness(relatedness=self.relatedness)
+	    self.datainput.addRelatedness(relatedness=self.relatedness)
     
-    def test_getRelatedness_mismatch_relatedness_and_samples_dimension(self):
+    def test_addRelatedness_mismatch_relatedness_and_samples_dimension(self):
         relatedness_samples=np.array(('S1', 'S2', 'S3'))
         with self.assertRaises(DataMismatch):
-	    self.datainput.getRelatedness(relatedness=self.relatedness,
+	    self.datainput.addRelatedness(relatedness=self.relatedness,
                                 relatedness_samples=relatedness_samples)
     
-    def test_getRelatedness_is_square_matrix(self):
+    def test_addRelatedness_is_square_matrix(self):
         relatedness = np.array(((1,2),(2,1), (3,3)))
         with self.assertRaises(FormatError):
-	    self.datainput.getRelatedness(relatedness=relatedness,
+	    self.datainput.addRelatedness(relatedness=relatedness,
                                 relatedness_samples=self.relatedness_samples)
-
-    def test_getRelatedness_relatedness_type(self):
-        relatedness=((1,2),(2,3))
-        with self.assertRaises(TypeError):
-	    self.datainput.getRelatedness(relatedness=relatedness,
-                                relatedness_samples=self.relatedness_samples)
-
-    def test_getRelatedness_relatedness_samples_type(self):
-        relatedness_samples=['S1', 'S2', 'S3']
-        with self.assertRaises(TypeError):
-	    self.datainput.getRelatedness(relatedness=self.relatedness,
-                                    relatedness_samples=relatedness_samples)
-    
-    def test_getRelatedness_fromOptions_wrongFileFormat(self):
-        self.options = Options()
-        self.datainputFromOptions = DataInput(self.options)
-        with self.assertRaises(FormatError):
-	    self.datainputFromOptions.getCovariates()
-    
-    def test_getRelatedness_fromOptions_FileNonExisting(self):
-        self.options = Options(file_relatedness="test.csv")
-        self.datainputFromOptions = DataInput(self.options)
-        with self.assertRaises(IOError):
-	    self.datainputFromOptions.getRelatedness()
 
     def test_traitstring_with_wrong_characters(self):
-        self.options = Options(traitstring="1.3")
-        self.datainputFromOptions = DataInput(self.options)
         with self.assertRaises(FormatError):
-            self.datainputFromOptions.subsetTraits()
+            self.datainput.subsetTraits(traitstring="1.3")
     
     def test_traitstring_with_traitnumber_gt_number_of_phenotypes(self):
-        self.options = Options(traitstring="1,1,5")
         phenotypes = np.array(((1,2,1,3), (1,3,1,3)))
         phenotype_ID = np.array(('ID2','ID2', 'ID3','ID4'))
         pheno_samples = np.array(('S1','S2'))
-        self.datainputFromOptions = DataInput(self.options)
-        self.datainputFromOptions.getPhenotypes(phenotypes=phenotypes, 
+        self.datainput = InputData(verbose=False)
+        self.datainput.addPhenotypes(phenotypes=phenotypes, 
                     pheno_samples=pheno_samples, phenotype_ID=phenotype_ID)
         with self.assertRaises(DataMismatch):
-            self.datainputFromOptions.subsetTraits()
+            self.datainput.subsetTraits(traitstring="1,1,5")
 
     def test_common_samples_no_overlap_pheno_relatedness(self):
-        self.datainput.pheno_samples = np.array(('S1','S2'))
-        self.datainput.relatedness_samples = np.array(('S3','S4'))
+        pheno_samples = np.array(('S10','S30'))
+        self.datainput = InputData(verbose=False)
+        self.datainput.addPhenotypes(phenotypes=self.phenotypes,
+                    pheno_samples=pheno_samples, phenotype_ID=self.phenotype_ID)
+        self.datainput.addRelatedness(relatedness=self.relatedness,
+                            relatedness_samples=self.relatedness_samples)
         with self.assertRaises(DataMismatch):
             self.datainput.commonSamples()
     
     def test_common_samples_no_overlap_pheno_relatedness_covs(self):
-        self.datainput.pheno_samples = np.array(('S1','S2'))
-        self.datainput.relatedness_samples = np.array(('S3','S4'))
-        self.datainput.covs_samples = np.array(('S5','S6'))
+        pheno_samples = np.array(('S10','S20'))
+        self.datainput = InputData(verbose=False)
+        self.datainput.addPhenotypes(phenotypes=self.phenotypes,
+                    pheno_samples=pheno_samples, phenotype_ID=self.phenotype_ID)
+        self.datainput.addCovariates(covariates=self.covariates,
+                                     covs_samples=self.covs_samples)
+        self.datainput.addRelatedness(relatedness=self.relatedness,
+                            relatedness_samples=self.relatedness_samples)
         with self.assertRaises(DataMismatch):
             self.datainput.commonSamples()
 
     def test_passing_of_transformation_method(self):
-        self.options = Options(transform="coxbox")
         phenotypes = np.array(((1,2,1,3), (1,3,1,3)))
         phenotype_ID = np.array(('ID2','ID2', 'ID3','ID4'))
         pheno_samples = np.array(('S1','S2'))
         covariates = np.array(((1,3), (3,1)))
         covs_samples = np.array(('S1','S2'))
-        self.datainputFromOptions = DataInput(self.options)
-        self.datainputFromOptions.getPhenotypes(phenotypes=phenotypes, 
+        self.datainputFromOptions = InputData(verbose=False)
+        self.datainputFromOptions.addPhenotypes(phenotypes=phenotypes, 
                     pheno_samples=pheno_samples, phenotype_ID=phenotype_ID)
-        self.datainputFromOptions.getCovariates(covariates=covariates, 
+        self.datainputFromOptions.addCovariates(covariates=covariates, 
                     covs_samples=covs_samples)
         with self.assertRaises(TypeError):
-            self.datainputFromOptions.regress_and_transform()
+            self.datainputFromOptions.transform(transform="coxbox")
 
     def test_regress_phenotypes_and_covs_are_different(self):
-        self.options = Options(transform="coxbox", regress=True)
         phenotypes = np.array(((1,2,1,3), (1,3,1,3)))
         phenotype_ID = np.array(('ID2','ID2', 'ID3','ID4'))
         pheno_samples = np.array(('S1','S2'))
         covariates = phenotypes
         covs_samples = np.array(('S1','S2'))
-        self.datainputFromOptions = DataInput(self.options)
-        self.datainputFromOptions.getPhenotypes(phenotypes=phenotypes, 
+        self.datainputFromOptions = InputData(verbose=False)
+        self.datainputFromOptions.addPhenotypes(phenotypes=phenotypes, 
                     pheno_samples=pheno_samples, phenotype_ID=phenotype_ID)
-        self.datainputFromOptions.getCovariates(covariates=covariates, 
+        self.datainputFromOptions.addCovariates(covariates=covariates, 
                     covs_samples=covs_samples)
         with self.assertRaises(DataMismatch):
-            self.datainputFromOptions.regress_and_transform()
-def soon():
-    ### make tests for ###
+            self.datainputFromOptions.regress(regress=True)
 
-    datalimmbo = DataLimmbo(datainput=datainput, options=dataparse.options)
-    resultsQ = datalimmbo.sampleCovarianceMatricesPP()
-    datalimmbo.combineBootstrap(resultsQ)
+        self.datainputFromOptions.addCovariates(covariates=self.covariates, 
+                    covs_samples=self.covs_samples)
+    
+    def test_transform_method_is_supported(self):
+        self.datainput.addPhenotypes(phenotypes=self.phenotypes, 
+                    pheno_samples=self.pheno_samples, 
+                    phenotype_ID=self.phenotype_ID)
+        with self.assertRaises(TypeError):
+            self.datainput.transform(type = "blub")
 
 if __name__ == '__main__':
     unittest.main()
---
+++ limmbo/io/test/test_reader.py
@@ -0,0 +1,63 @@
+import unittest as unittest
+import numpy as np
+import pandas as pd
+
+from limmbo.io.reader import ReadData
+from limmbo.io.reader import MissingInput
+from limmbo.io.reader import DataMismatch
+from limmbo.io.reader import FormatError
+
+class Reader(unittest.TestCase):
+
+    def setUp(self):
+        self.datainput = ReadData(verbose=False)
+
+    def test_getPhenotypes_missing_phenotype_file(self):
+        with self.assertRaises(MissingInput):
+	    self.datainput.getPhenotypes()
+    
+    def test_getPhenotypes_wrong_file_format(self):
+        with self.assertRaises(FormatError):
+	    self.datainput.getPhenotypes(file_pheno="pheno.unknownFormat")
+    
+    def test_getPhenotypes_file_cannot_be_opened(self):
+        with self.assertRaises(IOError):
+	    self.datainput.getPhenotypes(file_pheno="pheno.csv")
+
+    def test_getCovariates_wrong_file_format(self):
+        with self.assertRaises(FormatError):
+	    self.datainput.getCovariates(file_covariates="covs.unknownFormat")
+    
+    def test_getCovariates_file_cannot_be_opened(self):
+        with self.assertRaises(IOError):
+	    self.datainput.getCovariates(file_covariates="covs.csv")
+
+    def test_getGenotypes_missing_file(self):
+        with self.assertRaises(MissingInput):
+	    self.datainput.getGenotypes()
+    
+    def test_getGenotypes_wrong_file_format(self):
+        with self.assertRaises(FormatError):
+	    self.datainput.getGenotypes(
+                    file_genotypes="genos.unknownFormat")
+    
+    def test_getGenotypes_file_cannot_be_opened(self):
+        with self.assertRaises(IOError):
+	    self.datainput.getGenotypes(file_genotypes="genos.csv")
+    
+    def test_getPCs_wrong_file_format(self):
+        with self.assertRaises(FormatError):
+	    self.datainput.getPCs(file_pcs="PCAs.unknownFormat")
+    
+    def test_getVarianceComponents_file_cannot_be_opened(self):
+        with self.assertRaises(IOError):
+	    self.datainput.getVarianceComponents(file_Cg="cg.csv")
+    
+    def test_getVarianceComponents_file_cannot_be_opened(self):
+        with self.assertRaises(IOError):
+	    self.datainput.getVarianceComponents(file_Cg="cg.csv",
+            file_Cn="cn_csv")
+    
+
+if __name__ == '__main__':
+    unittest.main()
