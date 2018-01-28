eecf157cdc0cf811bc3b1b7f8914b1cfa810a7a0
Hannah <hannah.v.meyer@gmail.com>
2018-01-28 19:53:11 +0000

    put argparse back

--- doc/conf.py
+++ doc/conf.py
@@ -37,7 +37,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon']
+    'sphinx.ext.napoleon',
+    'sphinxarg.ext']
 napoleon_google_docstring = True
 
 # Add any paths that contain templates here, relative to this directory.
