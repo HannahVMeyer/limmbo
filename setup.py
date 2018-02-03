e90c0e7b9626e7587c7cfe00de9962680fee2277
Hannah <hannah.v.meyer@gmail.com>
2018-02-03 17:41:49 +0000

    include check for pypandoc

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup
 
 if __name__ == '__main__':
     readme = open('README.md').read()
-    import pypandoc
-    long_description = pypandoc.convert_text(
-        readme, 'rst', format='markdown_github')
+    try:
+        import pypandoc
+        long_description = pypandoc.convert_text(
+            readme, 'rst', format='markdown_github')
+    except (ImportError, RuntimeError, OSError):
+        long_description = readme
 
     setup(long_description=long_description,
           entry_points = {
