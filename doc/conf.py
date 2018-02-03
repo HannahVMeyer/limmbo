142a82b4edba0ade763284493e8946f42b4e676c
Hannah <hannah.v.meyer@gmail.com>
2018-02-03 17:45:24 +0000

    Adapt version

--- doc/conf.py
+++ doc/conf.py
@@ -64,7 +64,7 @@ author = u'Hannah V. Meyer'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.1.0'
+version = u'0.1.4'
 # The full version, including alpha/beta/rc tags.
 release = u'b'
 
