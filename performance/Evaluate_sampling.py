6197520bb3d35cc2f02f5436039880cbc75142ac
Hannah Meyer <hannah.v.meyer@gmail.com>
2019-08-07 16:51:09 +0000

    Update sampling performance results

--- performance/Evaluate_sampling.py
+++ performance/Evaluate_sampling.py
@@ -14,25 +14,26 @@ import seaborn as sns
 
 #The set cover of tuples is considered as trying to cover a square matrix of size
 def find_square(i_coord, j_coord, side_length):
-    return np.union1d(np.array(range(i_coord, i_coord+side_length)), np.array(range(j_coord, j_coord+side_length)))
+    return np.union1d(np.array(range(i_coord, i_coord+side_length)),
+        np.array(range(j_coord, j_coord+side_length)))
 
 #Relabel a subset so we don't need to recompute set covers for large m
 def relabel_subset(subset, permutation):
     return [permutation[x] for x in subset]
 
 ########
-#This method computes multiple set covers of tuples. 
-#To do so we consider instead covering a matrix is size number_of_traits*number_of_traits.   
+#This method computes multiple set covers of tuples.
+#To do so we consider instead covering a matrix is size number_of_traits*number_of_traits.
 ########
 def multiple_set_covers_all(number_of_traits, sample_size, number_of_covers,
         seed=2152):
     sample_size_t = sample_size
     if (sample_size % 2) != 0:#We make odd sized samples even and then fix the sampling size later.
         sample_size_t = sample_size -1
-    
+
     #Here we compute one set cover. After that we can generate the others based on this by relabelling
     #the matrix rows/cols with a permutation
-    i = 0 
+    i = 0
     j = 0
     used_subsets = list()
     while (j < number_of_traits):
@@ -54,11 +55,12 @@ def multiple_set_covers_all(number_of_traits, sample_size, number_of_covers,
     #(you can do it in a non-random way but the coverage will look cluster around the main diagonal
     if (sample_size % 2) !=0 :
         for i in range(0,len(used_subsets)):
-            used_subsets[i] = np.append(used_subsets[i], np.random.randint(0,number_of_traits))
-    
+            used_subsets[i] = np.append(used_subsets[i],
+                np.random.randint(0,number_of_traits))
+
     bootstrap_array = list()
     for num in range(0,number_of_covers):
-        order = np.random.permutation(number_of_traits)    
+        order = np.random.permutation(number_of_traits)
         for i in range(0,len(used_subsets)):
             bootstrap_array.append(relabel_subset(used_subsets[i], order))
             index = np.ix_(relabel_subset(used_subsets[i], order),
